48e315ad33bf9650da6ab9d85fa470acd7cc3e98
Olle Lukowski <ollelukowski@hotmail.com>
2025-01-06 17:34:41 +0100

    feat: ✨ create a new `WindowLoop` struct
    `WindowLoop` will be the most commonly used implementation of `MainLoop` for visual apps.

--- src/lib.rs
+++ src/lib.rs
@@ -3,4 +3,7 @@
 /// `use ottr::prelude::*;` to import the most common types, traits and functions.
 pub mod prelude {
     pub use ottr_main::prelude::*;
+
+    #[cfg(feature = "ottr_window")]
+    pub use ottr_window::prelude::*;
 }
